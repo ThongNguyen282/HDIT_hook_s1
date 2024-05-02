a3bb214e3e9788c87676476ca4bdf3f4bbd86ff9
ThongNC <nguyenhthong.235590@gmail.com>
2024-05-02 14:47:18 +0700

    #8-state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 
 function App() {
+  const [name, setName] = useState('thong');
+  const [address, setAddress] = useState('vn');
+  const handleClick = () => {
+    setName(address)
+
+  }
+
+  const changeAddress = (e) => {
+    setAddress(e.target.value)
+  }
   return (
     <div className="App">
       <Nav />
+      <header>
+        <p>Hello cac ban , I'm  {name}</p>
+        <p> I'm from  {address}</p>
+        <input type="text" value={address} onChange={(e) => { changeAddress(e) }} />
+        <button type="button" onClick={() => { handleClick() }}>Click Me</button>
+      </header>
+
     </div>
   );
 }
